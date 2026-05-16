16fce4e3af266cb518698095c1785e0b75b62bdb
LuisMa0121 <mayo_0121@hotmail.com>
2026-05-16 02:39:33 -0500

    Estructura de la seccion Mundo terminada

--- app/world/page.jsx
+++ app/world/page.jsx
@@ -1,3 +1,10 @@
+import WorldSection from "../../components/world/WorldSection";
+
 export default function WorldPage() {
-  return <h1>Módulo Mundo</h1>
+  return (
+    <main style={{ padding: '20px' }}>
+      <h1>Módulo Mundo</h1>
+      <WorldSection />
+    </main>
+  );
 }
