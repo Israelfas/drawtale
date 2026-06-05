27fe9067fb43671671db6bde87bdf149935e2e5d
Israelfas <marcelomejiauep@gmail.com>
2026-06-05 14:39:30 -0500

    Updates

--- _next/static/VAaLyTup6cZ9AqY82KWTb/_buildManifest.js
+++ _next/static/VAaLyTup6cZ9AqY82KWTb/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/drawtale//_next/:path+",
+        "destination": "/drawtale/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
